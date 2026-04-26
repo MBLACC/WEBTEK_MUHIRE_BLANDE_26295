d464e22b4d8300a36584c48559e101f15a959035
git blacc <blaccm797@gmail.com>
2026-04-27 01:38:04 +0200

    new project

--- install.js
+++
@@ -1,8 +0,0 @@
-const cp = require('child_process');
-console.log('Running npm install...');
-try {
-  cp.execSync('npm install --no-audit --no-fund', { stdio: 'inherit' });
-  console.log('Success');
-} catch (e) {
-  console.error('Failed', e.message);
-}
